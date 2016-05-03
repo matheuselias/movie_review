aa6d1fb1d37df9bf9c4796d5204945e081a44653
Matheus <matheus__e@hotmail.com>
2016-05-03 11:09:58 -0300

    Add crud Review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  respond_to :html, :json
+
+  def index
+    @reviews = Review.all
+    respond_with(@reviews)
+  end
+
+  def show
+    respond_with(@review)
+  end
+
+  def new
+    @review = Review.new
+    respond_with(@review)
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      respond_with(@review)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @review.update(review_params)
+    respond_with(@review)
+  end
+
+  def destroy
+    @review.destroy
+    respond_with(@review)
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
